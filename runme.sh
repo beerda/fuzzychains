3680dbd529a2355e25a11baed850a44461d6415c
Michal Burda <michal.burda@osu.cz>
2024-02-15 20:08:01 +0100

    Improved building scripts

--- runme.sh
+++ runme.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # disable CPU scaling (get rid of warning thrown by google benchmark
-sudo cpupower frequency-set --governor performance
+sudo cpupower frequency-set --governor performance > /dev/null
 
-../build-fuzzychains-Desktop_Qt_6_2_2_GCC_64bit-Release/fuzzychains
+build/apps/fuzzychains
 
 # enable CPU scaling again
-sudo cpupower frequency-set --governor powersave
+sudo cpupower frequency-set --governor powersave > /dev/null
 
